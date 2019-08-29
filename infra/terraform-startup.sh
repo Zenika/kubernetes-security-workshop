4ea24e5fbfa6a3dee69b7159bfd30f67947e483c
Pierre-Yves Aillet <pierre-yves.aillet-ext@rca.fr>
2019-08-29 11:02:20 +0200

    :ok_hand: adding kubectl

--- infra/terraform-startup.sh
+++ infra/terraform-startup.sh
@@ -7,3 +7,7 @@ apt install -y docker.io unzip
 curl -LO https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
 
 unzip terraform_0.12.7_linux_amd64.zip -d /usr/local/bin
+
+curl -L https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
+
+chmod +x /usr/local/bin/kubectl

2f216647ac150c2021934124e1892d1976f221b6
Pierre-Yves Aillet <pierre-yves.aillet-ext@rca.fr>
2019-08-29 10:56:48 +0200

    :bug: don't create the account.json here

--- infra/create-sa.sh
+++ infra/create-sa.sh
@@ -4,6 +4,3 @@ PROJECT=kubernetes-security-workshop
 
 gcloud iam service-accounts create terraform \
   --display-name "Terraform" --project $PROJECT
-
-gcloud iam service-accounts keys create account.json \
-  --iam-account terraform@$PROJECT.iam.gserviceaccount.com
